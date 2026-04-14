2f5cc23b6399f27f028c937ad52b1ab8531f5a90
Ramesh Boopathi <sanjai.ramesh@gmail.com>
2026-04-14 20:27:02 +0530

    Refactor MapCollection to include main method with HashMap operations

--- corejava/MapCollection.java
+++ corejava/MapCollection.java
@@ -1,4 +1,15 @@
 package corejava;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapCollection {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("A", 1);
+        map.computeIfAbsent("A", Integer::valueOf);
+        map.computeIfPresent("A", (k, v) -> v + 1);
+        System.out.println(map);
+    }
 }
